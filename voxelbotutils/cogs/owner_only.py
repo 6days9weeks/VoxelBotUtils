82fca5823ce271a747f175b2c1866b07ac0413c5
sora <60399731+6days9weeks@users.noreply.github.com>
2021-09-16 18:10:49 +0900

    Update owner_only.py

--- voxelbotutils/cogs/owner_only.py
+++ voxelbotutils/cogs/owner_only.py
@@ -278,7 +278,13 @@ class OwnerOnly(vbu.Cog, command_attrs={'hidden': False, 'add_slash_command': Fa
         if ret is None:
             # It might have printed something
             if stdout_value is not None:
-                await ctx.send(f'```py\n{stdout_value}\n```{self.get_execution_time(end_time, start_time)}')
+                if len(stdout_value) > 2000:
+                    try:
+                        return await ctx.send(self.get_execution_time(end_time, start_time), file=discord.File(io.StringIO(stdout_value), filename="ev.py"))
+                    except discord.HTTPException:
+                        return await ctx.send("I don't have permission to attach files here.")
+                else:
+                    return await ctx.send(f'```py\n{stdout_value}\n```{self.get_execution_time(end_time, start_time)}')
             return
 
         # If the function did return a value
