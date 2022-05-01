3fad8bad9e0ae6f0729f3a60258242f60c36cd16
6days9weeks <no-reply@6days9weeks.wtf>
2022-05-01 19:01:43 +0900

    stule

--- voxelbotutils/cogs/utils/checks/__init__.py
+++ voxelbotutils/cogs/utils/checks/__init__.py
@@ -1,10 +1,8 @@
 from .bot_is_ready import bot_is_ready
 from .is_bot_support import is_bot_support
 from .is_config_set import is_config_set
-from .is_upgrade_chat_subscriber import (
-    is_upgrade_chat_purchaser,
-    is_upgrade_chat_subscriber,
-)
+from .is_upgrade_chat_subscriber import (is_upgrade_chat_purchaser,
+                                         is_upgrade_chat_subscriber)
 from .is_voter import is_voter
 from .meta_command import meta_command
 
--- voxelbotutils/cogs/utils/types/bot_config_file.py
+++ voxelbotutils/cogs/utils/types/bot_config_file.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import Dict, List, Literal, Optional, TypedDict
 
-
 __all__ = (
     "_EventWebhookEvents",
     "_EventWebhook",
